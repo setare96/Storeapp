1cbe3f2f25a2139aa082dfb6d2a440f1b5eda5ea
amirhossein <aforootan75@gmail.com>
2018-01-04 20:50:20 +0330

    amir_test

--- app/src/main/java/com/setare/setare_96/store/Albums/Album.java
+++ app/src/main/java/com/setare/setare_96/store/Albums/Album.java
@@ -19,7 +19,7 @@ public class Album {
         this.sell_number = sell_number;
         this.upload_date = upload_date;
     }
-    //5555
+    //55555
 
     public String getName() {
         return name;
